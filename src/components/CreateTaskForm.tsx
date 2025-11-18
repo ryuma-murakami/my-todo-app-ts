93fba4accc4ecdd2e6544e93a344f76c39ddfe2b
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2025-11-18 22:55:16 +0900

    feat: Implement task creation

---
+++ src/components/CreateTaskForm.tsx
@@ -0,0 +1,44 @@
+import { Plus } from 'lucide-react';
+import { useRef } from 'react';
+
+type CreateTaskFormProps = {
+  onSubmit: (title: string) => void;
+};
+
+export function CreateTaskForm({ onSubmit }: CreateTaskFormProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputRef.current) {
+      return;
+    }
+
+    const inputValue = inputRef.current?.value.trim();
+    if (!inputValue) {
+      return;
+    }
+
+    onSubmit(inputValue);
+
+    inputRef.current.value = '';
+  };
+
+  return (
+    <form className="flex gap-0.5" onSubmit={handleSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="新しいタスクを入力してください"
+        className="grow rounded-s border border-gray-300 p-2 bg-white"
+      />
+      <button
+        type="submit"
+        className="rounded-e bg-blue-600 p-2 transition-colors hover:bg-blue-800 disabled:cursor-not-allowed disabled:bg-gray-400"
+        aria-label={'タスクを作成する'}
+      >
+        <Plus className="text-white" />
+      </button>
+    </form>
+  );
+}
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,14 +1,40 @@
-import { dummyTaskList } from '../data/dummyTaskList';
+import { useState } from 'react';
+import { CreateTaskForm } from './CreateTaskForm';
 import { TaskItem } from './TaskItem';
 
 export function TaskList() {
+  type Task = {
+    id: number;
+    title: string;
+    status: 'notStarted' | 'completed' | 'trashed';
+  };
+
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  const createTask = (title: string) => {
+    setTaskList((prevTaskList) => {
+      const newTask: Task = {
+        id: Date.now(),
+        title,
+        status: 'notStarted',
+      };
+
+      return [...prevTaskList, newTask];
+    });
+  };
+
   return (
     <div className="relative">
+      <div className="sticky top-0 flex flex-col items-end gap-2 bg-slate-100 px-10 py-5">
+        <div className="w-full">
+          <CreateTaskForm onSubmit={createTask} />
+        </div>
+      </div>
       <div className="space-y-3 px-10 pb-10">
-        {dummyTaskList.length === 0 ? (
+        {taskList.length === 0 ? (
           <p className="text-center text-sm">タスクがありません</p>
         ) : (
-          dummyTaskList.map((task) => <TaskItem key={task.id} task={task} />)
+          taskList.map((task) => <TaskItem key={task.id} task={task} />)
         )}
       </div>
     </div>
